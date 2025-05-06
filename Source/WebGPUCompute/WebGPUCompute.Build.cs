661e696ea27fe380fb976a8ef5ca4096108bf790
getnamo <getnamo@gmail.com>
2025-05-06 10:13:04 -0700

    third party include from https://github.com/gfx-rs/wgpu-native

--- Source/WebGPUCompute/WebGPUCompute.Build.cs
+++ Source/WebGPUCompute/WebGPUCompute.Build.cs
@@ -1,27 +1,48 @@
 // Copyright 2025-current Getnamo. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class WebGPUCompute : ModuleRules
 {
+	private string WebGpuThirdPartyPath
+	{
+		get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/WebGPU")); }
+	}
+
+	private string WebGpuIncludePath
+	{
+		get { return Path.GetFullPath(Path.Combine(WebGpuThirdPartyPath,  "Include")); }
+	}
+
+	private string WebGpuLibPath
+	{
+		get { return Path.GetFullPath(Path.Combine(WebGpuThirdPartyPath, "Lib")); }
+	}
+
+	private string WebGpuBinariesPath
+	{
+		get { return Path.GetFullPath(Path.Combine(WebGpuThirdPartyPath, "Binaries")); }
+	}
+
 	public WebGPUCompute(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
 		PublicIncludePaths.AddRange(
 			new string[] {
 				// ... add public include paths required here ...
 			}
 			);
-				
-		
+
+
 		PrivateIncludePaths.AddRange(
 			new string[] {
 				// ... add other private include paths required here ...
 			}
 			);
-			
-		
+
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -29,8 +50,8 @@ public class WebGPUCompute : ModuleRules
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
-			
-		
+
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -41,13 +62,24 @@ public class WebGPUCompute : ModuleRules
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
-		
-		
+
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
 				// ... add any modules that your module loads dynamically here ...
 			}
 			);
+
+		PublicIncludePaths.Add(WebGpuIncludePath);
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			string Win64LibPath = Path.Combine(WebGpuLibPath, "Win64");
+			string Win64BinariesPath = Path.Combine(WebGpuBinariesPath, "Win64");
+
+			PublicAdditionalLibraries.Add(Path.Combine(Win64LibPath, "wgpu_native.lib"));
+			RuntimeDependencies.Add("$(BinaryOutputDir)/wgpu_native.dll", Path.Combine(Win64BinariesPath, "wgpu_native.dll"));
+		}
 	}
 }
