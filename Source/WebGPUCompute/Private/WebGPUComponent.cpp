88fac9ac25b6a9e4bc8368ad75800131de1e0df7
getnamo <getnamo@gmail.com>
2025-05-06 13:26:57 -0700

    extract source out to test live changing code in bp
    - run shader should in theory work
    - it's not optimal since the whole pipe gets built and torn down, but it's a start

--- Source/WebGPUCompute/Private/WebGPUComponent.cpp
+++ Source/WebGPUCompute/Private/WebGPUComponent.cpp
@@ -120,57 +120,17 @@ public:
 	}
 
 	//todo: export source and auto-bind
-	void RunShader()
+	void RunShader(const FString& Source)
 	{
+		const char* SourceBuffer = TCHAR_TO_ANSI(*Source);
 		WGPUShaderSourceWGSL sourceDesc = {};
 		sourceDesc.chain.sType = WGPUSType_ShaderSourceWGSL;
-
-		const char* Source = (R"(
-@group(0)
-@binding(0)
-var<storage, read_write> v_indices: array<u32>; // this is used as both input and output for convenience
-
-// The Collatz Conjecture states that for any integer n:
-// If n is even, n = n/2
-// If n is odd, n = 3n+1
-// And repeat this process for each new n, you will always eventually reach 1.
-// Though the conjecture has not been proven, no counterexample has ever been found.
-// This function returns how many times this recurrence needs to be applied to reach 1.
-fn collatz_iterations(n_base: u32) -> u32 {
-	var n : u32 = n_base;
-	var i : u32 = 0u;
-	loop{
-		if (n <= 1u) {
-			break;
-		}
-		if (n % 2u == 0u) {
-			n = n / 2u;
-		}
-		else {
-			// Overflow? (i.e. 3*n + 1 > 0xffffffffu?)
-			if (n >= 1431655765u) {   // 0x55555555u
-				return 4294967295u;   // 0xffffffffu
-			}
-
-			n = 3u * n + 1u;
-		}
-		i = i + 1u;
-	}
-	return i;
-}
-
-@compute
-@workgroup_size(1)
-fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-	v_indices[global_id.x] = collatz_iterations(v_indices[global_id.x]);
-}
-		)");
 
 		uint32_t numbers[] = { 1, 2, 3, 4 };
 		uint32_t numbers_size = sizeof(numbers);
 		uint32_t numbers_length = numbers_size / sizeof(uint32_t);
 
-		sourceDesc.code = { Source, WGPU_STRLEN };
+		sourceDesc.code = { SourceBuffer, WGPU_STRLEN};
 
 		WGPUShaderModuleDescriptor shaderDesc = {};
 		shaderDesc.label = { "shader.wgsl", WGPU_STRLEN };
@@ -308,10 +268,16 @@ fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
 		wgpuInstanceRelease(Instance);
 	}
 
+	bool HasStarted() 
+	{
+		return Instance != nullptr;
+	}
+
 	WGPUInstance Instance = nullptr;
 	WGPUAdapter Adapter = nullptr;
 	WGPUDevice Device = nullptr;
 	WGPUQueue Queue = nullptr;
+	
 };
 
 UWebGPUComponent::UWebGPUComponent(const FObjectInitializer& ObjectInitializer)
@@ -322,6 +288,8 @@ UWebGPUComponent::UWebGPUComponent(const FObjectInitializer& ObjectInitializer)
 
 UWebGPUComponent::~UWebGPUComponent()
 {
+	Internal->Shutdown();
+
 	delete Internal;
 	Internal = nullptr;
 }
@@ -347,23 +315,78 @@ void UWebGPUComponent::Test()
 
 	//From: https://eliemichel.github.io/LearnWebGPU/getting-started/hello-webgpu.html#lit-6
 
-	
-	Internal->Startup();
+	if (!Internal->HasStarted())
+	{
+		Internal->Startup();
 
-	// Display the object (WGPUInstance is a simple pointer, it may be
-	// copied around without worrying about its size).
-	UE_LOG(LogTemp, Log, TEXT("WGPU instance: %p"), Internal->Instance);
+		// Display the object (WGPUInstance is a simple pointer, it may be
+		// copied around without worrying about its size).
+		UE_LOG(LogTemp, Log, TEXT("WGPU instance: %p"), Internal->Instance);
 
+		Internal->InspectAdapter(Internal->Adapter);
+	}
 
-	Internal->InspectAdapter(Internal->Adapter);
+	const char* RawSource = (R"(
+@group(0)
+@binding(0)
+var<storage, read_write> v_indices: array<u32>; // this is used as both input and output for convenience
 
-	Internal->RunShader();
+// The Collatz Conjecture states that for any integer n:
+// If n is even, n = n/2
+// If n is odd, n = 3n+1
+// And repeat this process for each new n, you will always eventually reach 1.
+// Though the conjecture has not been proven, no counterexample has ever been found.
+// This function returns how many times this recurrence needs to be applied to reach 1.
+fn collatz_iterations(n_base: u32) -> u32 {
+	var n : u32 = n_base;
+	var i : u32 = 0u;
+	loop{
+		if (n <= 1u) {
+			break;
+		}
+		if (n % 2u == 0u) {
+			n = n / 2u;
+		}
+		else {
+			// Overflow? (i.e. 3*n + 1 > 0xffffffffu?)
+			if (n >= 1431655765u) {   // 0x55555555u
+				return 4294967295u;   // 0xffffffffu
+			}
 
-	Internal->Shutdown();
+			n = 3u * n + 1u;
+		}
+		i = i + 1u;
+	}
+	return i;
+}
+
+@compute
+@workgroup_size(1)
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+	v_indices[global_id.x] = collatz_iterations(v_indices[global_id.x]);
+}
+		)");
+
+	FString Source = FString(RawSource);
+
+	Internal->RunShader(Source);
 
 	UE_LOG(LogTemp, Log, TEXT("## Test end. ##"));
 }
 
 void UWebGPUComponent::RunShader(const FString& ShaderSource)
 {
+
+	if (!Internal->HasStarted())
+	{
+		Internal->Startup();
+
+		// Display the object (WGPUInstance is a simple pointer, it may be
+		// copied around without worrying about its size).
+		UE_LOG(LogTemp, Log, TEXT("WGPU instance: %p"), Internal->Instance);
+
+		Internal->InspectAdapter(Internal->Adapter);
+	}
+
+	Internal->RunShader(ShaderSource);
 }
