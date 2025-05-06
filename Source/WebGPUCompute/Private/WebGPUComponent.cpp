d6ca830c26dee3a5b3da89f24149a8b45f62a62d
getnamo <getnamo@gmail.com>
2025-05-06 13:04:47 -0700

    shader readability
    - WGPU_STRLEN for null terminated strings

--- Source/WebGPUCompute/Private/WebGPUComponent.cpp
+++ Source/WebGPUCompute/Private/WebGPUComponent.cpp
@@ -121,47 +121,48 @@ public:
 		WGPUShaderSourceWGSL sourceDesc = {};
 		sourceDesc.chain.sType = WGPUSType_ShaderSourceWGSL;
 
-		const char* Source = (R"(@group(0)
-			@binding(0)
-			var<storage, read_write> v_indices: array<u32>; // this is used as both input and output for convenience
+		const char* Source = (R"(
+@group(0)
+@binding(0)
+var<storage, read_write> v_indices: array<u32>; // this is used as both input and output for convenience
 
-		// The Collatz Conjecture states that for any integer n:
-		// If n is even, n = n/2
-		// If n is odd, n = 3n+1
-		// And repeat this process for each new n, you will always eventually reach 1.
-		// Though the conjecture has not been proven, no counterexample has ever been found.
-		// This function returns how many times this recurrence needs to be applied to reach 1.
-		fn collatz_iterations(n_base: u32) -> u32 {
-			var n : u32 = n_base;
-			var i : u32 = 0u;
-			loop{
-				if (n <= 1u) {
-					break;
-				}
-				if (n % 2u == 0u) {
-					n = n / 2u;
-				}
-				else {
-					// Overflow? (i.e. 3*n + 1 > 0xffffffffu?)
-					if (n >= 1431655765u) {   // 0x55555555u
-						return 4294967295u;   // 0xffffffffu
-					}
-
-					n = 3u * n + 1u;
-				}
-				i = i + 1u;
+// The Collatz Conjecture states that for any integer n:
+// If n is even, n = n/2
+// If n is odd, n = 3n+1
+// And repeat this process for each new n, you will always eventually reach 1.
+// Though the conjecture has not been proven, no counterexample has ever been found.
+// This function returns how many times this recurrence needs to be applied to reach 1.
+fn collatz_iterations(n_base: u32) -> u32 {
+	var n : u32 = n_base;
+	var i : u32 = 0u;
+	loop{
+		if (n <= 1u) {
+			break;
+		}
+		if (n % 2u == 0u) {
+			n = n / 2u;
+		}
+		else {
+			// Overflow? (i.e. 3*n + 1 > 0xffffffffu?)
+			if (n >= 1431655765u) {   // 0x55555555u
+				return 4294967295u;   // 0xffffffffu
 			}
-			return i;
-		}
 
-		@compute
-			@workgroup_size(1)
-			fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-			v_indices[global_id.x] = collatz_iterations(v_indices[global_id.x]);
+			n = 3u * n + 1u;
 		}
+		i = i + 1u;
+	}
+	return i;
+}
+
+@compute
+@workgroup_size(1)
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+	v_indices[global_id.x] = collatz_iterations(v_indices[global_id.x]);
+}
 		)");
 
-		sourceDesc.code = { Source, strlen(Source)};
+		sourceDesc.code = { Source, WGPU_STRLEN };
 
 		WGPUShaderModuleDescriptor shaderDesc = {};
 		shaderDesc.label = { "shader.wgsl", WGPU_STRLEN };
@@ -170,6 +171,9 @@ public:
 		WGPUShaderModule shader_module = wgpuDeviceCreateShaderModule(Device, &shaderDesc);
 
 		assert(shader_module);
+
+
+		wgpuShaderModuleRelease(shader_module);
 	}
 
 	//release all memories used
